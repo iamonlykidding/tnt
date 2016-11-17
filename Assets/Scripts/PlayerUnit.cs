6c2d2b55715837ea18e4e3062e717ef95976ed41
Danny <djculwell@gmail.com>
2016-11-17 05:17:51 -0800

    initial

---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoonSharp.Interpreter;
+
+public class PlayerUnit : MonoBehaviour {
+	string State;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(State == "Move"){
+		//		if player clicks a moveable tile
+		//		move unit there and either allow attack
+		//		or end turn
+		//}
+	}
+
+	void Move(){
+		State = "Move";
+
+		GameObject[] Terrains = 
+			GameObject.FindGameObjectsWithTag ("Ground");
+		foreach (GameObject Terrain in Terrains) {
+			//if distance < UnitSpeed turn tile blue
+			//and mark it as valid move
+		}
+	}
+}
